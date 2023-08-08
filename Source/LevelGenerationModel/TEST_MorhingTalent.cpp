bcc08ed250d1ef2573b57c47353a9ff41668e4fd
vdaleke <vdalekesmirnov@gmail.com>
2023-08-08 19:25:43 +0300

    Add arena actors source

---
+++ Source/LevelGenerationModel/TEST_MorhingTalent.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TEST_MorhingTalent.h"
+
+// Sets default values
+ATEST_MorhingTalent::ATEST_MorhingTalent()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ATEST_MorhingTalent::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATEST_MorhingTalent::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
